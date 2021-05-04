acb3dcb4a0b54220d3cd7a10bcfe515f2779d6b7
NJ <45119220+NJCodeGIT@users.noreply.github.com>
2021-05-05 02:03:31 +0530

    base code commit

---
+++ ReadMe.md
@@ -0,0 +1,28 @@
+https://www.geeksforgeeks.org/how-to-run-cron-jobs-in-node-js/
+
+# Descriptors with their ranges:
+
+Seconds (optional): 0 – 59
+Minute: 0 – 59
+Hour: 0 – 23
+Day of the Month: 1 – 31
+Month: 1 – 12
+Day of the week: 0 – 7 (0 and 7 both represent Sunday)
+Examples:
+
+(*/10 * * * * *) – Runs on every 10 seconds
+(*/10 * * * *) – Runs on every 10 minutes
+(* * 21 * *) – Runs 21th of every month
+(0 8 * * 1) – Runs 8 AM on every Monday
+
+# npm packages
+npm install express node-cron
+npm install nodemailer
+npm install dotenv
+npm install --save @sendgrid/mail
+
+# To get district Id for Kerala
+
+Please see districts.json file
+
+# SendGrid Access Key need to change in .env file for getting email 
---
+++ index.js
@@ -0,0 +1,96 @@
+const cron = require("node-cron");
+const express = require("express");
+const https = require('https')
+const sgMail = require('@sendgrid/mail')
+
+require('dotenv').config()
+
+app = express(); // Initializing app
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1;
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = '0' + dd;
+}
+if (mm < 10) {
+  mm = '0' + mm;
+}
+var today = dd + '-' + mm + '-' + yyyy;
+
+const options = {
+    hostname: 'cdn-api.co-vin.in',
+    port: 443,
+    // path: '/api/v2/appointment/sessions/public/calendarByPin?pincode=695023&date=04-05-2021',
+    path: `/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${process.env.DISTRICT_ID}&date=${today}`,
+    method: 'GET'
+}
+
+// Creating a cron job which runs on every 10 second
+cron.schedule("*/1 * * * *", function () {
+    console.log("running a task every 1 minute");
+    
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+      
+        res.on('data', d => {
+          var emailBody = "";
+          var slotResult = JSON.parse(d);
+
+          if (slotResult != undefined && slotResult.centers != undefined)
+          {
+              slotResult.centers.forEach(center => {
+                  if (center.sessions != undefined)
+                  {
+                    center.sessions.forEach(session => {
+                        if (session.available_capacity == 0)
+                        {
+                          emailBody += `<p><strong>${session.vaccine}</strong> is available for <strong>${center.fee_type}</strong></p>`;
+                          emailBody += `<p><strong>Location&nbsp; </strong>: ${center.pincode} || ${center.name} ||  ${center.address} || ${center.block_name}</p>`;
+                          emailBody += `<p><strong>Date&nbsp; &nbsp; &nbsp; &nbsp; </strong>: ${session.date}.</p>`;
+                          emailBody += `<p><strong>Slot&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</strong>: ${session.available_capacity}.</p>`;
+                          emailBody += `<p>-----------------------------------------------------------------------------------------------</p>`;
+                          emailBody += `<p>&nbsp;</p>`;
+                        }
+                    });
+                  }
+              });
+          }
+
+          if (emailBody != "")
+          {
+            const msg = {
+              to: 'nijumn.tech@gmail.com', // Change to your recipient
+              from: 'nijumn.tech@gmail.com', // Change to your verified sender
+              subject: 'Co-WIN Slot Availability Alert',
+              text: 'Co-WIN Slot Availability Alert',
+              html: emailBody,
+            }
+  
+            sgMail.send(msg).then(() => {
+              console.log('Email sent!!!!!!!!!!!!!!!!!!!!');
+            }).catch((error) => {
+              console.error(error);
+            });
+          }
+          else
+          {
+            console.log(`Email not sent`);
+          }
+
+        })
+      })
+      
+      req.on('error', error => {
+        console.error(error)
+      })
+      
+      req.end()
+
+});
+
+app.listen();
